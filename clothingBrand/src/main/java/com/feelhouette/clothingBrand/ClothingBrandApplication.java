61467f22de8a661b7e5a5e75c34aa9c0bbc207cb
theAllanMzungu <theallanmzungu@gmail.com>
2026-03-08 09:28:22 +0300

    Tracing with Micrometer, OpenTelemetry, and Grafana Tempo  - OpenTelemetry, a vendor-neutral framework for collecting telemetry data (logs, metrics, traces)  - We use Grafana Tempo as the backend tracing tool, which allows Grafana to query traces  -  Much like Micrometer's API for metrics, Micrometer Tracing provides a facade for using more popular tracing libraries

--- clothingBrand/src/main/java/com/feelhouette/clothingBrand/ClothingBrandApplication.java
+++ clothingBrand/src/main/java/com/feelhouette/clothingBrand/ClothingBrandApplication.java
@@ -2,12 +2,15 @@ package com.feelhouette.clothingBrand;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 public class ClothingBrandApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ClothingBrandApplication.class, args);
+		Hooks.enableAutomaticContextPropagation();
 	}
 
 }
